51e9d842488f33419f03b4c50b7c986f79f727cb
Anudeep Reddy <95900205+Crazydeepu@users.noreply.github.com>
2023-04-27 19:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let display = document.getElementById('timer');
+
+let date = new Date().toLocalDateString();
+let time = new Date().toLocalDateString();
+display.innerHtml = date+" , "+time;
