1b23f2e89b432a55b6ec2b51f5afccbf8df18723
Anudeep Reddy <95900205+Crazydeepu@users.noreply.github.com>
2023-05-22 19:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-//your JS code here. If required.
-let display = document.getElementById('timer');
+function startTime(){
+        const today = new Date();
+        let day = today.getUTCDate();
+        let mon = today.getMonth()+1;
+        let year = today.getFullYear();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let s = today.getSeconds();
+        am_pm = "AM";
 
-let date = new Date().toLocalDateString();
-let time = new Date().toLocalDateString();
-display.innerHtml = date+", "+time;
+        if(h > 12) {
+            h -= 12;
+            am_pm = "PM";
+        }
+        if(h == 0) {
+            h = 12;
+            am_pm = "AM";
+        }
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('timer').innerHTML = day+"/"+mon+"/"+year+", "+h+":"+m+":"+s + " "+am_pm;
+        setTimeout(startTime,1000);
+    }
+
+    function checkTime(i){
+        if(i<10){
+            i = "0" + i;
+        }
+        return i;
+    }
